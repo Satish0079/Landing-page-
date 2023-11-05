baacf2aa451be15cfe67aa827b2e9462f31b9b4d
Satish Gupta <125724143+Satish0079@users.noreply.github.com>
2023-11-05 15:45:02 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ import "./About.css";
 const TypewriterBox = () => {
   const [paragraphs, setParagraphs] = useState([]);
   const paragraphsToType = [
-    "A hackathon is an exciting and collaborative event that brings together creative individuals, often including programmers, designers, and innovators, for a focused period to develop innovative solutions to real-world problems. These intensive coding marathons typically span a day or more, during which participants work diligently to build software, apps, or hardware prototypes. Hackathons foster an atmosphere of innovation, learning, and competition, pushing participants to think outside the box and come up with inventive solutions.. Overall, hackathons are a hub of creativity and innovation, where like-minded individuals come together to create, learn, and make a difference.",
+    "A hackathon is an exciting and collaborative event that brings together creative individuals, often including programmers, designers, and innovators, for a focused period to develop innovative solutions to real-world problems. Overall, hackathons are a hub of creativity and innovation, where like-minded individuals come together to create, learn, and make a difference.",
   ];
   const [currentParagraphIndex, setCurrentParagraphIndex] = useState(0);
   const [currentText, setCurrentText] = useState("");
