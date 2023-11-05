3a39e9e6dbe7b180d81fcccd7c20ee954dcbcfec
Satish Gupta <125724143+Satish0079@users.noreply.github.com>
2023-11-05 13:48:15 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -3,68 +3,43 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <div className="">
-      <footer className="footer4">
-        <div className="container">
-          <a href="./" className="brand">
-            Brand
-          </a>
-          <ul className="menu">
-            <li className="Footerli">
-              <a href="./">Home</a>
-            </li>
-            <li className="Footerli">
-              <a href="./">About</a>
-            </li>
-            <li className="Footerli">
-              <a href="./">Contact</a>
-            </li>
-            <li className="Footerli">
-              <a href="./">Blog</a>
-            </li>
-            <li className="Footerli">
-              <a href="./">Services</a>
-            </li>
-            <li className="Footerli">
-              <a href="./">Pricing</a>
-            </li>
-          </ul>
-          <ul className="media-icons">
-            <li className="Footerli">
-              <a href="./">
-                <i className="fab fa-facebook"></i>
-              </a>
-            </li>
-            <li className="Footerli">
-              <a href="./">
-                <i className="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li className="Footerli">
-              <a href="./">
-                <i className="fab fa-instagram"></i>
-              </a>
-            </li>
-            <li className="Footerli">
-              <a href="./">
-                <i className="fab fa-tumblr"></i>
-              </a>
-            </li>
-            <li className="Footerli">
-              <a href="./">
-                <i className="fab fa-linkedin"></i>
-                {/* <img src="background.jpg" alt=""> */}
-              </a>
-            </li>
-          </ul>
-          <p className="Footerp">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid,
-            quibusdam officia doloremque cumque veritatis perspiciatis aliquam
-            porro necessitatibus earum dolorum cupiditate vero similique? A
-            incidunt quod voluptatibus fuga. Porro, possimus!
-          </p>
+   <footer>
+        <div class="container">
+            <div class="sec">
+                <h2>About Us</h2>
+                <p>A hackathon is an exciting and collaborative event that brings together creative individuals, often including programmers, designers, and innovators, for a focused period to develop innovative solutions to real-world problems.</p>
+                <ul class="sci">
+                    <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fab fa-discord"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                </ul>
+            </div>
+            <div class="sec quiz">
+                <h2>Support</h2>
+                <ul>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Code of Conduct</a></li>
+                    <li><a href="#">Help</a></li>
+                </ul>
+            </div>
+            <div class="sec contact">
+                <h2>Contact Us</h2>
+                <ul class="info">
+                    <li>
+                        <span><i class="fas fa-phone"></i></span>
+                        <p><a href="tel:+12345645556">+0121510021566</a></p>
+                    </li>
+                    <li>
+                        <span><i class="fas fa-envelope"></i></span>
+                        <p><a href="mailto:hackanova3.0@gmail.com">hackanova3.0@gmail.com</a></p>
+                    </li>
+                </ul>
+            </div>
         </div>
-      </footer>
+    </footer>
+    <div class="copyrightText">
+        <p>Copyright © 2023 Hackanova 3.0. All Rights Reserved</p>
     </div>
   );
 }
